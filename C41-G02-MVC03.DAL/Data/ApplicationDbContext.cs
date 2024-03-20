fa051d52e4efe04fb8c2a5e77caaf6cb52b004cf
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-03-20 18:39:04 +0200

    DAL for Department Model

---
+++ C41-G02-MVC03.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using C41_G02_MVC03.DAL.Data.Configurations;
+using C41_G02_MVC03.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C41_G02_MVC03.DAL.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .;Database = MVC01; Trusted_Connection = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ C41-G02-MVC03.DAL/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C41_G02_MVC03.DAL.Models
+{
+    //Model
+    internal class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; } 
+
+    }
+}
