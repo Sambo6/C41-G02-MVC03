84286adb9a484dbba03cd484dff2af6b583a9400
MUhammed_AbdUllah <67861134+Sambo6@users.noreply.github.com>
2024-03-20 23:34:09 +0200

    Refactor DBContext With DPI Connection with  AppSetting + Add Migrations in DAL

--- C41-G02-MVC03.PL/Startup.cs
+++ C41-G02-MVC03.PL/Startup.cs
@@ -16,12 +16,12 @@ namespace C41_G02_MVC03.PL
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the DepInj container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,15 +30,20 @@ namespace C41_G02_MVC03.PL
 
 
             //services.AddTransient<ApplicationDbContext>(); //(Every object) has (request and connections)
+
             services.AddScoped<ApplicationDbContext>(); // (One Object) for (requests)
+
             //services.AddSingleton<ApplicationDbContext>(); //Connection with Data base will be opened for time 
 
             //services.AddScoped<DbContextOptions<ApplicationDbContext>>();
 
-            // Dependance Injection
-            services.AddDbContext<ApplicationDbContext>(Options => Options.UseSqlServer("Server = .;Database = MVC01; Trusted_Connection = True")
-
-                );
+            // Dependence Injection
+            services.AddDbContext<ApplicationDbContext>(Options =>
+            {
+                
+                Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
